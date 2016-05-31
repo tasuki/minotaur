248370f4ac370a573b557eca39da292ec11fdfd6
Vit Brunner <vit.brunner@gmail.com>
2016-05-31 10:31:00 +0200

    More underscores

--- src/main/scala/minotaur/model/Board.scala
+++ src/main/scala/minotaur/model/Board.scala
@@ -15,17 +15,17 @@ case class Board(
     val locationsToCheck: Seq[Option[Location]] = direction match {
       case North => Seq(
         location.neighbor(North),
-        location.neighbor(North).flatMap(l => l.neighbor(West)))
+        location.neighbor(North).flatMap(_.neighbor(West)))
       case South => Seq(Some(location), location.neighbor(West))
       case East => Seq(Some(location), location.neighbor(North))
       case West => Seq(
         location.neighbor(West),
-        location.neighbor(West).flatMap(l => l.neighbor(North)))
+        location.neighbor(West).flatMap(_.neighbor(North)))
     }
 
     locationsToCheck.flatten
-      .filter(l => l.allowsWallPlacement)
-      .filter(l => walls contains Wall(l, direction.orientation.opposite))
+      .filter(_.allowsWallPlacement)
+      .filter(walls contains Wall(_, direction.orientation.opposite))
       .length == 0
   }
 
