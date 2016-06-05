c2a975642ed3bcb2564c9d0502cef7c2ba61286b
Vit Brunner <vit.brunner@gmail.com>
2016-06-05 14:22:53 +0200

    Better separation of specs

---
+++ src/test/scala/minotaur/model/GameState.scala
@@ -0,0 +1,123 @@
+package minotaur.model
+
+import org.specs2.mutable.Specification
+import minotaur.io.BoardReader
+
+class GameStateSpec extends Specification {
+  "Sample board with black to move" should {
+    val board = BoardReader.fromString("""
+      |+   +   +   +   +
+      |
+      |+---+---+   +   +
+      |    | o
+      |+   +   +   +   +
+      |    | x |
+      |+   +   +   +   +
+      |        |
+      |+   +   +   +   +
+    """.stripMargin.trim)
+    val gs = GameState(
+      board, Map(Black -> 4, White -> 3), Black
+    )
+
+    "list the possible moves" in {
+      gs.getChildren.map(_.board) === Set(
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    | o   x
+          |+   +   +   +   +
+          |    |   |
+          |+   +   +   +   +
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    | o
+          |+   +   +   +   +
+          |    |   |
+          |+   +   +   +   +
+          |      x |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |            |
+          |+---+---+   +   +
+          |    | o     |
+          |+   +   +   +   +
+          |    | x |
+          |+   +   +   +   +
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    | o
+          |+   +   +---+---+
+          |    | x |
+          |+   +   +   +   +
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    | o     |
+          |+   +   +   +   +
+          |    | x |   |
+          |+   +   +   +   +
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    | o
+          |+   +   +   +   +
+          |    | x |
+          |+---+---+   +   +
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    | o
+          |+   +   +   +   +
+          |    | x |
+          |+   +   +---+---+
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    | o
+          |+   +   +   +   +
+          |    | x |   |
+          |+   +   +   +   +
+          |        |   |
+          |+   +   +   +   +
+        """.stripMargin.trim)
+      )
+    }
+  }
+}
