aa541329bf2225c286d00ce4bf3200551ca83b65
Vit Brunner <vit.brunner@gmail.com>
2016-10-10 16:00:01 +0200

    Test moves carry over stuff

--- src/test/scala/minotaur/model/Move.scala
+++ src/test/scala/minotaur/model/Move.scala
@@ -30,4 +30,76 @@ class MoveSpec extends Specification {
       move.wins === false
     }
   }
+
+  "Wall placement" should {
+    val board = BoardReader.fromString("""
+      |+   +   +   +   +
+      |
+      |+---+---+   +   +
+      |    |
+      |+   +   +   +   +
+      |    | o | x
+      |+   +   +   +   +
+      |        |
+      |+   +   +   +   +
+    """.stripMargin.trim)
+    val gs = GameState(
+      board, Map(Black -> 4, White -> 3), Black
+    )
+    val move = WallPlacement(Wall(Location(8, board.boardType), Horizontal), gs)
+
+    "generate the correct new state" in {
+      move.play === GameState(
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    |
+          |+   +   +   +   +
+          |    | o | x
+          |+---+---+   +   +
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+        Map(Black -> 3, White -> 3),
+        White
+      )
+    }
+  }
+
+  "Pawn movement" should {
+    val board = BoardReader.fromString("""
+      |+   +   +   +   +
+      |
+      |+---+---+   +   +
+      |    |
+      |+   +   +   +   +
+      |    | o | x
+      |+   +   +   +   +
+      |        |
+      |+   +   +   +   +
+    """.stripMargin.trim)
+    val gs = GameState(
+      board, Map(Black -> 4, White -> 3), Black
+    )
+    val move = PawnMovement(Location(6, board.boardType), gs)
+
+    "generate the correct new state" in {
+      move.play === GameState(
+        BoardReader.fromString("""
+          |+   +   +   +   +
+          |
+          |+---+---+   +   +
+          |    |     x
+          |+   +   +   +   +
+          |    | o |
+          |+   +   +   +   +
+          |        |
+          |+   +   +   +   +
+        """.stripMargin.trim),
+        Map(Black -> 4, White -> 3),
+        White
+      )
+    }
+  }
 }
