e49cdf915e0629e1de76740274d877ab89c0a136
Vit Brunner <vit.brunner@gmail.com>
2018-04-12 18:28:43 +0200

    Display more move stats

--- src/main/scala/minotaur/io/NodePrinter.scala
+++ src/main/scala/minotaur/io/NodePrinter.scala
@@ -3,7 +3,7 @@ package minotaur.io
 import minotaur.mcts.Node
 
 object NodePrinter {
-  def printTopMoves(parent: Node, topX: Int = 5): List[String] = {
+  def printTopMoves(parent: Node, topX: Int = 15): List[String] = {
     parent.bestChildren(topX).toList.map { node =>
       node.move.map { move =>
         node.toString + " " + MovePrinter.print(move)
