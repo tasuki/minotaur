a93fc6675fee5945ed017864290fd38215cd7ddb
Vit Brunner <vit.brunner@gmail.com>
2018-08-15 22:33:08 +0200

    Fixy fix

--- src/test/scala/minotaur/io/RecommenderTest.scala
+++ src/test/scala/minotaur/io/RecommenderTest.scala
@@ -10,7 +10,8 @@ class RecommenderTest extends Specification {
       val record = "e2;e8;e3;f8;e4;d4h;d3h;e3v;d4;b4h;b3h;f7;f6h;g7;c4;h6v;g6v;f7;b4"
       val game = QuoridorStrats.importGame(record)
 
-      val firstMove = Recommender.recommend(game).head
+      val recommender = new Recommender("http://localhost:8008")
+      val firstMove = recommender.recommend(game).head
       QuoridorStrats.toQuoridorStrats(firstMove) === "e7"
     }
   }
